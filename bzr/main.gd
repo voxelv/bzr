58ceb614680b89197fb4741da8335fd9140e1e77
Tim Slippy, voxelv <slippytim@gmail.com>
2018-07-08 13:50:52 -0700

    fullscreen on HTML5

--- bzr/main.gd
+++ bzr/main.gd
@@ -3,6 +3,9 @@ extends Node
 onready var vbox = $GridContainer/VBoxContainer
 
 func _ready():
+	if OS.get_name() == "HTML5":
+		OS.set_window_maximized(true)
+	
 	for child in vbox.get_children():
 		if child is Button:
 			child.connect("pressed", self, "_on_button_press", [child.get_position_in_parent()])
